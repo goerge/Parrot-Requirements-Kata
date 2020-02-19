b4b110fa6698ee81ba3cc89dbe2282d0be0928d4
Peter Kofler <peter.kofler@code-cop.org>
2020-02-19 13:48:55 +0100

    Test list.

--- Java/src/test/java/parrot/ParrotTest.java
+++ Java/src/test/java/parrot/ParrotTest.java
@@ -16,3 +16,33 @@ class ParrotTest {
         assertEquals(0.0, speed);
     }
 }
+
+/*
+ * type
+ * - European
+ * - African
+ * - Norwegian
+ * - other (wrong input)
+ * - null (wrong input)
+ * 
+ * European ... 1
+ * - regardless of input -> 12
+ * 
+ * African ... 3
+ * - numberOfCoconuts 0 -> 12
+ * - numberOfCoconuts 1 -> 3
+ *                         boundary is between
+ * - numberOfCoconuts 2 -> 0 (min)
+ * - numberOfCoconuts -1 -> 21 (wrong input)
+ * 
+ * Norwegian ... 4
+ * - isNailed true -> 0
+ * - isNailed false, 0 -> 0
+ * - isNailed false, 1 -> 12
+ * - isNailed false, 2 -> 24 (boundary)
+ * - isNailed false, >2 -> 24 (min)
+ * - isNailed false, -1 -> -12  (wrong input)
+ * 
+ * null
+ * - -> NullPointer
+ */
