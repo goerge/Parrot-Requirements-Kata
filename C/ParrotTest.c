5d1180f71521f05e822a15030e88a47498fec1a7
Peter Kofler <peter.kofler@code-cop.org>
2020-02-20 22:33:13 +0100

    Add sample CMocka test, ignore cmocka.

---
+++ C/ParrotTest.c
@@ -0,0 +1,26 @@
+#include <stdlib.h> /* size_t for mocka */
+#include <setjmp.h> /* jmp_buf for mocka */
+#include <stdarg.h> /* va_start for mocka */
+#include <cmocka.h>
+#include "Parrot.h"
+
+static void test_foo(void** state)
+{
+    Parrot parrot;
+    float speed;
+
+    init_parrot(&parrot, EUROPEAN, 0, 0.0, false);
+
+    speed = get_speed(&parrot);
+
+    assert_float_equal(0.0, speed, 0.0001);
+}
+
+int main(void)
+{
+    const struct CMUnitTest test_suite[] = { /* */
+        cmocka_unit_test(test_foo), /* */
+    };
+
+    return cmocka_run_group_tests(test_suite, NULL, NULL);
+}
