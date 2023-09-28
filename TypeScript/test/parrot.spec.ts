b256676aadf63c54ad551512b91c1d76d3acbfb5
Peter Kofler <peter.kofler@code-cop.org>
2023-09-28 17:36:48 +0200

    Do some formatting. Sigh.

--- TypeScript/test/parrot.spec.ts
+++ TypeScript/test/parrot.spec.ts
@@ -4,27 +4,49 @@ import { Parrot, ParrotTypes } from "../src/parrot";
 describe("Parrot", () => {
     describe("getSpeed", () => {
         it("should return the base speed for a European parrot", () => {
+            // Arrange
             const parrot = new Parrot(ParrotTypes.EUROPEAN, 0, 0.0, false);
-            expect(parrot.getSpeed()).to.equal(12);
+
+            // Act
+            const actualSpeed = parrot.getSpeed();
+
+            // Assert
+            expect(actualSpeed).to.equal(12);
         });
 
         it("should return the base speed minus load factor multiplied by number of coconuts for an African parrot", () => {
+            // Arrange
             const numberOfCoconuts = 2;
             const parrot = new Parrot(ParrotTypes.AFRICAN, numberOfCoconuts, 0.0, false);
-            const expectedSpeed = 12 - 9 * numberOfCoconuts;
-            expect(parrot.getSpeed()).to.equal(expectedSpeed);
+
+            // Act
+            const actualSpeed = parrot.getSpeed();
+
+            // Assert
+            expect(actualSpeed).to.equal(12 - 9 * numberOfCoconuts);
         });
 
         it("should return 0 for a nailed Norwegian Blue parrot", () => {
+            // Arrange
             const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 0.0, true);
-            expect(parrot.getSpeed()).to.equal(0);
+
+            // Act
+            const actualSpeed = parrot.getSpeed();
+
+            // Assert
+            expect(actualSpeed).to.equal(0);
         });
 
         it("should return the base speed with voltage for an unnailed Norwegian Blue parrot", () => {
+            // Arrange
             const voltage = 0.5;
             const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, voltage, false);
-            const expectedSpeed = Math.min(24, voltage * 12);
-            expect(parrot.getSpeed()).to.equal(expectedSpeed);
+
+            // Act
+            const actualSpeed = parrot.getSpeed();
+
+            // Assert
+            expect(actualSpeed).to.equal(Math.min(24, voltage * 12));
         });
     });
 });
