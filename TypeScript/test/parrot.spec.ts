bd4af683d376b4d4e080f2f96d803fedb4abcc51
Peter Kofler <peter.kofler@code-cop.org>
2023-09-28 17:56:53 +0200

    Manually add some test cases. Kind of OK to work with.

--- TypeScript/test/parrot.spec.ts
+++ TypeScript/test/parrot.spec.ts
@@ -49,5 +49,85 @@ describe("Parrot", () => {
             // Assert
             expect(actualSpeed).to.equal(Math.min(24, voltage * 12));
         });
+
+        // --- 2nd ---
+
+        it("should return the base speed for an African parrot with no coconuts", () => {
+            // Arrange
+            const numberOfCoconuts = 0;
+            const parrot = new Parrot(ParrotTypes.AFRICAN, numberOfCoconuts, 0.0, false);
+
+            // Act
+            const actualSpeed = parrot.getSpeed();
+
+            // Assert
+            expect(actualSpeed).to.equal(12);
+        });
+
+        it("should return 0 for a European parrot with negative numberOfCoconuts", () => {
+            // Arrange
+            const numberOfCoconuts = -2;
+            const parrot = new Parrot(ParrotTypes.EUROPEAN, numberOfCoconuts, 0.0, false);
+
+            // Act
+            const actualSpeed = parrot.getSpeed();
+
+            // Assert
+            expect(actualSpeed).to.equal(12);
+        });
+
+        xit("should return 0 for a Norwegian Blue parrot with negative voltage", () => {
+            // Arrange
+            const voltage = -0.5;
+            const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, voltage, false);
+
+            // Act
+            const actualSpeed = parrot.getSpeed();
+
+            // Assert
+            expect(actualSpeed).to.equal(-6);
+        });
+
+        it("should return the maximum speed of 24 for a Norwegian Blue parrot with voltage greater than 2", () => {
+            // Arrange
+            const voltage = 2.5;
+            const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, voltage, false);
+
+            // Act
+            const actualSpeed = parrot.getSpeed();
+
+            // Assert
+            const expectedSpeed = Math.min(24, voltage * 12);
+            expect(actualSpeed).to.equal(expectedSpeed);
+        });
+
+        // --- manual add ---
+
+        it("should return the maximum speed of 24 for a Norwegian Blue parrot with voltage greater than 2", () => {
+            // Arrange
+            const voltage = Number.MAX_VALUE;
+            const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, voltage, false);
+
+            // Act
+            const actualSpeed = parrot.getSpeed();
+
+            // Assert
+            expect(actualSpeed).to.equal(24);
+        });
+
+        it("should return the maximum speed for an African parrot with maximum coconuts", () => {
+            // Arrange
+            const numberOfCoconuts = Number.MAX_SAFE_INTEGER;
+            const parrot = new Parrot(ParrotTypes.AFRICAN, numberOfCoconuts, 0.0, false);
+
+            // Act
+            const actualSpeed = parrot.getSpeed();
+
+            // Assert
+            // const expectedSpeed = 12 - 9 * (numberOfCoconuts - 1); <- why -1
+            const expectedSpeed = 0;
+            expect(actualSpeed).to.equal(expectedSpeed);
+        });
+
     });
 });
